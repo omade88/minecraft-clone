6222ed3147ac2db9e0bbaf13629fd38185680657
cloud <omadetech@gmail.com>
2026-01-06 20:59:22 -0500

    Updated camera controls to improve user experience

--- src/engine/camera.cpp
+++ src/engine/camera.cpp
@@ -1,24 +1,4 @@
 #include "camera.h"
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-
-class Camera {
-public:
-    Camera(float fov, float aspect, float near, float far);
-    void move(const glm::vec3& offset);
-    glm::mat4 getViewMatrix() const;
-
-private:
-    glm::vec3 position;
-    glm::vec3 front;
-    glm::vec3 up;
-    float fov;
-    float aspect;
-    float near;
-    float far;
-};
-
-// Implementation of Camera methods
 
 Camera::Camera(float fov, float aspect, float near, float far)
     : position(0.0f, 0.0f, 3.0f), front(0.0f, 0.0f, -1.0f), up(0.0f, 1.0f, 0.0f),
--- src/engine/camera.h
+++ src/engine/camera.h
@@ -1,11 +1,23 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
 class Camera {
 public:
-    Camera();
-    void move(float deltaX, float deltaY, float deltaZ);
+    Camera(float fov = 45.0f, float aspect = 16.0f/9.0f, float near = 0.1f, float far = 100.0f);
+    void move(const glm::vec3& offset);
     glm::mat4 getViewMatrix() const;
 
 private:
     glm::vec3 position;
     glm::vec3 front;
     glm::vec3 up;
+    float fov;
+    float aspect;
+    float near;
+    float far;
 };
+
+#endif // CAMERA_H
